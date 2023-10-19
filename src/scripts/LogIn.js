84907c210f2400c633c51f7affab8c345dfd66ee
Henrique20o <twoguini@gmail.com>
2023-10-19 16:06:53 -0300

    commit

---
+++ src/scripts/LogIn.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+
+    $("#login-form").submit(function(e){
+
+        e.preventDefault();
+
+        let nme = null;
+        let password = null;
+
+        var User = {
+            name: $("#user-inpt").val(),
+            pass: $("#password-inpt").val(),
+        };
+
+        var DBUser = {
+            name: ["admin", "Gay1234"],
+            pass: ["admin", "gay123"],
+        };
+
+        DBUser.name.forEach(e => {
+            if (e === User.name) {
+                nme = e;
+            }
+        });
+
+        DBUser.pass.forEach(e => {
+            if (e === User.pass) {
+                password = e;
+            }
+        });
+        
+        if (nme === User.name && password === User.pass) {
+            alert("ta tudo certo mesmo, passou")
+            window.location.replace('pages/about.html')
+        }
+        else{
+            alert("não achei")
+        }
+
+    });
+
+});
