964023e983e826a9c130ccd964ac1db673c00bd6
Henrique20o <twoguini@gmail.com>
2023-10-19 16:25:50 -0300

    commit

--- src/scripts/validateQuests.js
+++ src/scripts/validateQuests.js
@@ -1,27 +1,31 @@
 $(document).ready(function () {
 
-    $("form").submit(function () { 
+    $("form").submit(function (e) { 
+
+        e.preventDefault();
 
         var questionAnswers = [];
 
         questionAnswers.push($("#question-1-selection").val(),$("#question-2-selection").val(), $("#question-3-selection").val(), $("#question-4-selection").val(), $("#question-5-selection").val())
-        
-        alert(questionAnswers)
 
         let correctAnswers = ["b","a","c","d","a"];
 
         var totalPoints = 0;
+        let wrongQuestions = [];
 
         $(questionAnswers).each(function(index, value){
             if (value === correctAnswers[index]){
                 totalPoints++;
             }
             else{
-                alert("Questão: " + (Number(index)+1) + " Está Errada.")
+                wrongQuestions.push(Number(index)+1)
             }
         });
 
-        alert("Acertou: " + totalPoints);
+        $("#result").css("visibility", "visible")
+
+        $("#wrong-questions").text("As questões " + wrongQuestions + " Estão erradas")
+        $("#total-grade").text(totalPoints + "/5");
     });
 
 });
