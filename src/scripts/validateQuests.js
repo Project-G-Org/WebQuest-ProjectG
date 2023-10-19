1febf6a1fc361c330d1753335045a6ce2eb155f4
Henrique20o <twoguini@gmail.com>
2023-10-19 14:01:30 -0300

    Commit

--- src/scripts/validateQuests.js
+++ src/scripts/validateQuests.js
@@ -1,3 +1,27 @@
-/**
- * Validação dos questionarios
- */
+$(document).ready(function () {
+
+    $("form").submit(function () { 
+
+        var questionAnswers = [];
+
+        questionAnswers.push($("#question-1-selection").val(),$("#question-2-selection").val(), $("#question-3-selection").val(), $("#question-4-selection").val(), $("#question-5-selection").val())
+        
+        alert(questionAnswers)
+
+        let correctAnswers = ["b","a","c","d","a"];
+
+        var totalPoints = 0;
+
+        $(questionAnswers).each(function(index, value){
+            if (value === correctAnswers[index]){
+                totalPoints++;
+            }
+            else{
+                alert("Questão: " + (Number(index)+1) + " Está Errada.")
+            }
+        });
+
+        alert("Acertou: " + totalPoints);
+    });
+
+});
