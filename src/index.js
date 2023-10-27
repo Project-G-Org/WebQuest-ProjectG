1135f058f39a2603cd573d13991e0c9e0812258a
Twoguini <twoguini@gmail.com>
2023-10-26 22:54:16 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -4,47 +4,66 @@ const cors = require("cors");
 const {v4: uuid} = require("uuid");
 const _ = require("lodash");
 const path = require("path");
+const handlebars = require("express-handlebars");
 
 const app = xpress();
 
-const publicPath = path.join(__dirname,'public');
+// SECTION: ========================= Directories ========================= 
+    const publicPath = path.join(__dirname,'public'); // NOTE: Public Dir
+// !SECTION ===============================================================
 
-app.use(xpress.json());
-app.use("/style", xpress.static(publicPath + "/style"));
-app.use("/script", xpress.static(publicPath + "/script"));
-app.use("/img", xpress.static(publicPath + "/img"));
+// SECTION: =========================== Config ============================
+    app.use(xpress.json()); // NOTE: To Handle Json
+    app.use("/style", xpress.static(publicPath + "/style")); // NOTE: Css Styles
+    app.use("/script", xpress.static(publicPath + "/script")); // NOTE: Js Scripts
+    app.use("/img", xpress.static(publicPath + "/img")); // NOTE: imgs
 
-app.get('/', (req,res) =>{
+    app.engine("handlebars", handlebars.engine({defaultLayout: 'main'})); // NOTE: HandleBars as Template engine
+    app.set('view engine', 'handlebars'); // NOTE: ...
+// !SECTION ===============================================================
 
-    res.sendFile(`${publicPath}/webPages/index.html`);
-});
-
-app.get('/questions', (req, res) =>{
-
-    const quests = ["Beibe beibe do birulaibe?", "oldley candralaray do meu?"];
-
-    res.json({
-        question: _.sample(quests)
+// SECTION: ============================ Routes ===========================
+    app.get('/', (req,res) =>{
+        // NOTE: LogIn Page
+        res.render('login');
     });
-});
 
-app.post("/login", (req, res) => {
+    app.post("/", (req, res) => {
+        // NOTE: Request Name and Username
+        const id = uuid();
+        const username = req.body.username;
+        const password = req.body.password;
 
-    const id = uuid();
-    const username = req.body.username;
-    const password = req.body.password;
+        if(!username || !password){
+            return res.sendStatus(400);
+        }
 
-    if(!username || !password){
-
-        return res.sendStatus(400);
-    }
-
-    res.status(201).json({
-        id: id,
-        username: username,
-        password: password 
+        res.status(201).json({
+            id: id,
+            username: username,
+            password: password 
+        });
     });
-});
+
+    app.get('/home', (req, res) =>{
+        // NOTE: Home Page
+        res.render('home');
+    });
+
+    app.get('/material', (req, res) =>{
+        // NOTE: Material Page
+        res.render('material');
+    });
+    app.get('/signup', (req, res) =>{
+        // NOTE: Signup Page
+        res.render('signUp');
+    });
+
+    app.get('/questions', (req, res) =>{
+        // NOTE: Questions Form Page
+        res.render('quests');
+    });
+// !SECTION ===============================================================
 
 app.listen(8080, ()=>{
 
